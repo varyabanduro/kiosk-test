1e34422cfbba76e761dd7a62fd512147dbd37d4f
varyabanduro <122829439+varyabanduro@users.noreply.github.com>
2025-05-25 15:55:23 +0300

    Update test-install.sh

--- test-install.sh
+++ test-install.sh
@@ -75,6 +75,10 @@ remove_kiosk() {
   fi
   
   local delete_files=('/usr/local/bin/kiosk' '/etc/systemd/system/kiosk.service' '/usr/local/etc/kiosk')
+  if ! systemctl stop kiosk.service; then
+    echo 'error: Failed to stop kiosk service.'
+    exit 1
+  fi
   
   if ! systemctl disable kiosk.service; then
     echo 'error: Failed to disable kiosk service.'
