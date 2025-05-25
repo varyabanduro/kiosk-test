9de1c37d99282d98cd4531427346f3e0509f3b54
varyabanduro <122829439+varyabanduro@users.noreply.github.com>
2025-05-25 14:17:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import vlc
+import tkinter as tk
+
+import os
+import time
+from dotenv import load_dotenv
+
+import requests
+import logging
+import threading
+
+
 while True:
     print('Hello World')
